cdfcb6529a8634fac0d8362d341b7497b6224f05
OhadCarmel <121317208+OhadCarmel@users.noreply.github.com>
2023-03-26 13:25:46 +0300

    Question 1

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,18 +1,51 @@
 import * as R from "ramda";
+import { pipe } from "ramda";
 
 const stringToArray = R.split("");
+//"Ohad Carmel Is ThE kinG"
 
 /* Question 1 */
-export const countLetters: undefined = undefined
+export const countLetters: (s : string) => { [index: string]: number } = (s: string): { [index: string]: number } => {
+    const pipeFunctions : (input: string) => {[index: string]: number} = pipe(
+        R.replace(/\s/g, ''), 
+        (str: string) => R.split("")(str), 
+        (arr: string[]) => R.countBy(R.toLower)(arr));
+    return pipeFunctions(s);
+  }
+
+
+const s : string = "Ohad Carmel Is ThE kinG";
+console.log(countLetters(s));
+console.log(s);
 
 /* Question 2 */
-export const isPaired: undefined = undefined
+export const isPaired: (s: string)=> boolean = (s: string): boolean => {
+    const pipeFunctions : (input: string) => string[] = pipe(
+        R.replace(/\s/g, ''), 
+        (str: string) => R.split("")(str));
+    
+    const parenthesesFilter = (str: string) => str ==="(" || str === ")" || str === "[" || str === "]" || str === "{" || str === "}";
+    //Ramda.takeLast() - returns the last n elements of the given list or string.
+    //Ramda.dropLast() - returns a new list or string without the last n elements.
+    //Ramda.slice() - returns a new list or string containing the elements between the start and end indexes.
+    //Ramda.take() - returns the first n elements of the given list or string.
+    //Ramda.drop() - returns a new list or string without the first n elements.
+
+    //Ramda.split() - splits a string into an array of strings based on the given separator.
+    return true;
+}
 
 /* Question 3 */
 export type WordTree = {
     root: string;
     children: WordTree[];
-}
+} 
 
-export const treeToSentence : undefined = undefined
+export const treeToSentence : (w: WordTree)=> string = (w: WordTree): string => {
+
+
+    
+
+    return "";
+}
 
