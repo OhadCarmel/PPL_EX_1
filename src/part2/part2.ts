5d5ce773a2d26b1d135a2ac16c82150fa8ce4ee2
OhadCarmel <121317208+OhadCarmel@users.noreply.github.com>
2023-03-26 13:58:36 +0300

    Question 3

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -5,26 +5,21 @@ const stringToArray = R.split("");
 //"Ohad Carmel Is ThE kinG"
 
 /* Question 1 */
-export const countLetters: (s : string) => { [index: string]: number } = (s: string): { [index: string]: number } => {
-    const pipeFunctions : (input: string) => {[index: string]: number} = pipe(
-        R.replace(/\s/g, ''), 
-        (str: string) => R.split("")(str), 
+export const countLetters: (s: string) => { [index: string]: number } = (s: string): { [index: string]: number } => {
+    const pipeFunctions: (input: string) => { [index: string]: number } = pipe(
+        R.replace(/\s/g, ''),
+        (str: string) => R.split("")(str),
         (arr: string[]) => R.countBy(R.toLower)(arr));
     return pipeFunctions(s);
-  }
-
-
-const s : string = "Ohad Carmel Is ThE kinG";
-console.log(countLetters(s));
-console.log(s);
+}
 
 /* Question 2 */
-export const isPaired: (s: string)=> boolean = (s: string): boolean => {
-    const pipeFunctions : (input: string) => string[] = pipe(
-        R.replace(/\s/g, ''), 
+export const isPaired: (s: string) => boolean = (s: string): boolean => {
+    const pipeFunctions: (input: string) => string[] = pipe(
+        R.replace(/\s/g, ''),
         (str: string) => R.split("")(str));
-    
-    const parenthesesFilter = (str: string) => str ==="(" || str === ")" || str === "[" || str === "]" || str === "{" || str === "}";
+
+    const parenthesesFilter = (str: string) => str === "(" || str === ")" || str === "[" || str === "]" || str === "{" || str === "}";
     //Ramda.takeLast() - returns the last n elements of the given list or string.
     //Ramda.dropLast() - returns a new list or string without the last n elements.
     //Ramda.slice() - returns a new list or string containing the elements between the start and end indexes.
@@ -39,13 +34,34 @@ export const isPaired: (s: string)=> boolean = (s: string): boolean => {
 export type WordTree = {
     root: string;
     children: WordTree[];
-} 
-
-export const treeToSentence : (w: WordTree)=> string = (w: WordTree): string => {
-
-
-    
-
-    return "";
 }
 
+export const treeToSentence: (tree: WordTree) => string = (tree: WordTree): string => {
+
+    const s: string[] = tree.children.map((child: WordTree) => treeToSentence(child)).filter(sentence => sentence !== '');
+    return [tree.root, ...s].join(' ');
+}
+
+const t1: WordTree = {
+    root: "Hello",
+    children: [
+        {
+            root: "students",
+            children: [
+                {
+                    root: "how",
+                    children: []
+                }
+            ]
+        },
+        {
+            root: "are",
+            children: []
+        },
+        {
+            root: "you?",
+            children: []
+        },
+    ]
+}
+console.log(treeToSentence(t1)); // ==> Hello students how are you?
