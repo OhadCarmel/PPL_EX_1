6e5a678fbd6aa6c3dffb19e27ffc8a1d2a1e2b78
1Yuval <reuvyuv@post.bgu.ac.il>
2023-04-01 17:08:25 +0300

    PART 2 Q 2

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -2,7 +2,6 @@ import * as R from "ramda";
 import { pipe } from "ramda";
 
 const stringToArray = R.split("");
-//"Ohad Carmel Is ThE kinG"
 
 /* Question 1 */
 export const countLetters: (s: string) => { [index: string]: number } = (s: string): { [index: string]: number } => {
@@ -16,56 +15,35 @@ export const countLetters: (s: string) => { [index: string]: number } = (s: stri
 /* Question 2 */
 
 
-export const isPaired = (arr: string[]): boolean =>{
-    type Pair = {
-        open: string;
-        close: string;
-      };
+type Pair = {
+    open: string;
+    close: string;
+  };
 
-    const pairs: Pair[] = [
-        { open: "(", close: ")" },
-        { open: "{", close: "}" },
-        { open: "[", close: "]" },
-      ];
+const pairs: Pair[] = [
+    { open: "(", close: ")" },
+    { open: "{", close: "}" },
+    { open: "[", close: "]" },
+  ];
+
+const isOpener = (char: string) => pairs.some((pair) => pair.open === char);
+
+const isCloser = (char: string) => pairs.some((pair) => pair.close === char);
+
+const matchingPair = (open: string | undefined, close:string) => pairs.find((pair) => pair.open === open && pair.close === close);
+
+export const isPaired: (s: string) => boolean = pipe(
+        stringToArray,
+        R.filter((char: string) => isOpener(char) || isCloser(char)),
+        R.reduce((acc: string[], curr: string) => isOpener(curr) ? [...acc, curr] : matchingPair(R.last(acc), curr) ? R.init(acc) : [...acc, curr], []),
+        R.isEmpty
+    )
+
    
-    const open = ["(", "[", "{"];
-    const close = [")", "]", "}"];
-    
-    const symetric = R.reduce(
-        (acc: string[], char: string) => {
 
-            const isOpener = (char: string) => pairs.some((pair) => pair.open === char);
-
-            const isCloser = (char: string) => pairs.some((pair) => pair.close === char);
-
-            if(isOpener(char)){
-                return [...acc, char];
-            }
-            if(isCloser(char)){
-                const lastChar: (acc: string[]) => string | undefined = (acc: string[]) => R.last(acc);
-                const last = lastChar(acc);
-                if(last){
-                    const matchingPair: (pair: Pair | undefined, last: string, char: string) => Pair| undefined = (pair: Pair | undefined, last: string, char: string) => pairs.find((pair) => pair.open === last && pair.close === char);
-                    const match = matchingPair(pairs.find((pair) => pair.open === last), last, char);
-                    if(match){
-                        const retvalue = (acc:string) => R.init(acc) === undefined ? [] : R.init(acc);
-                        return retvalue;
-                    }
-                    else return acc;
-                    
-                }
-            }
-            return acc;
-            
-        },
-        [],
-        arr
-    );
-    return symetric.length === 0;
-}
         
 
-/* Question 3 *
+// Question 3 *
 export type WordTree = {
     root: string;
     children: WordTree[];
@@ -76,4 +54,8 @@ export const treeToSentence: (tree: WordTree) => string = (tree: WordTree): stri
     const s: string[] = tree.children.map((child: WordTree) => treeToSentence(child)).filter(sentence => sentence !== '');
     return [tree.root, ...s].join(' ');
 }
+
+console.log(isPaired("This is ([some]) {text}"));
+
+
 
