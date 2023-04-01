cff08d1742b37d760e7ac1e8dbfc03af6cc58d0d
OhadCarmel <121317208+OhadCarmel@users.noreply.github.com>
2023-04-01 20:08:07 +0300

    final

--- src/part3/find.ts
+++ src/part3/find.ts
@@ -33,27 +33,3 @@ export const returnSquaredIfFoundEven_v3 : (arr: number[])=> number = (arr: numb
     return either(findResult(isEven, arr), (x: number) => x * x, (e: string) => -1);
 }
 
-
-// const data = [1, 2, 3, 4, 5];
-// const data2 = [1, 3, 5];
-// const data3 = [1, -2, 3, -4, 5];
-// const data4 = [1, -2.2, 3, -4.1, 5];
-// console.log("data: ", data);
-// console.log(returnSquaredIfFoundEven_v1(data));
-// console.log(returnSquaredIfFoundEven_v2(data));
-// console.log(returnSquaredIfFoundEven_v3(data));
-
-// console.log("data2: ", data2);
-// console.log(returnSquaredIfFoundEven_v1(data2));
-// console.log(returnSquaredIfFoundEven_v2(data2));
-// console.log(returnSquaredIfFoundEven_v3(data2));
-
-// console.log("data3: ", data3);
-// console.log(returnSquaredIfFoundEven_v1(data3));
-// console.log(returnSquaredIfFoundEven_v2(data3));
-// console.log(returnSquaredIfFoundEven_v3(data3));
-
-// console.log("data4: ", data4);
-// console.log(returnSquaredIfFoundEven_v1(data4));
-// console.log(returnSquaredIfFoundEven_v2(data4));
-// console.log(returnSquaredIfFoundEven_v3(data4));
